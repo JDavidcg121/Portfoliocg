3723e754053789aa740bebc97a5ccaf2616bbb2b
Jason <jasoncg.cr@gmail.com>
2026-04-10 17:04:43 -0600

    General modifications

--- projects/agrotec/cypress-automation/cypress/e2e/Edits menu.cy.js
+++ projects/agrotec/cypress-automation/cypress/e2e/Edits menu.cy.js
@@ -1,20 +1,31 @@
-describe('Menú Ediciones', () => {
+describe('Edits menu', () => {
   beforeEach(() => {
     cy.viewport(1280, 800);
     cy.visit('https://agroteccr.com/');
   });
 
-it('allows access to the Edits menu', () => {
-  cy.contains('a', /ediciones/i)
-    .should('be.visible');
+  it('allows access to the Edits menu and scrolls twice', () => {
+    cy.contains('a', /ediciones/i)
+      .should('be.visible');
 
-  cy.wait(2000);
+    cy.wait(2000);
 
-  cy.contains('a', /ediciones/i)
-    .click({ force: true });
+    cy.contains('a', /ediciones/i)
+      .click({ force: true });
 
     cy.url().then((url) => {
       expect(url.toLowerCase()).to.match(/ediciones|revista|agrotec/);
     });
+      cy.wait(500); 
+      cy.scrollTo(0, 200);
+        cy.wait(500);
+    
+    cy.scrollTo(0, 400);
+    cy.wait(500); 
+    cy.scrollTo(0, 600);
+     cy.wait(500); 
+    cy.scrollTo(0, 900);
+     cy.wait(500); 
+    cy.scrollTo(0, 1500);
   });
 });
